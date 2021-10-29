e68ed877a53a5d4c6e5809f52e191bb5f65f261f
Davyjones <ekenedavidson15@gmail.com>
2021-10-29 06:54:05 +0100

    merged

--- src/main/java/com/sabi/logistics/api/controllers/BrandController.java
+++ src/main/java/com/sabi/logistics/api/controllers/BrandController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@SuppressWarnings("All")
 @Validated
 @RestController
 @RequestMapping(Constants.APP_CONTENT+"brand")
