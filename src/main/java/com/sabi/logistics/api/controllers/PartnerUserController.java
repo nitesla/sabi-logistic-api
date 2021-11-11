a313278db3ba790dce3650ae471e5b745ea8de29
Davyjones <ekenedavidson15@gmail.com>
2021-11-11 19:27:46 +0100

    data seed

--- src/main/java/com/sabi/logistics/api/controllers/PartnerUserController.java
+++ src/main/java/com/sabi/logistics/api/controllers/PartnerUserController.java
@@ -2,30 +2,31 @@ package com.sabi.logistics.api.controllers;
 
 
 import com.sabi.framework.dto.responseDto.Response;
+import com.sabi.framework.models.User;
 import com.sabi.framework.utils.Constants;
 import com.sabi.framework.utils.CustomResponseCode;
 import com.sabi.logistics.core.dto.request.PartnerUserRequestDto;
 import com.sabi.logistics.core.dto.response.PartnerUserResponseDto;
 import com.sabi.logistics.service.services.PartnerUserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @SuppressWarnings("ALL")
 @RestController
 @RequestMapping(Constants.APP_CONTENT+"partneruser")
 public class PartnerUserController {
+
     private final PartnerUserService partnerUserService;
 
 
 
-    public PartnerUserController(PartnerUserService PartnerUserService) {
-        this.partnerUserService = PartnerUserService;
+    public PartnerUserController(PartnerUserService partnerUserService) {
+        this.partnerUserService = partnerUserService;
 
     }
 
@@ -45,6 +46,24 @@ public class PartnerUserController {
 
 
 
+    @GetMapping("/page")
+    public ResponseEntity<Response> getPartnerUser(@RequestParam(value = "firstName",required = false)String firstName,
+                                                   @RequestParam(value = "phone",required = false)String phone,
+                                                   @RequestParam(value = "email",required = false)String email,
+                                                   @RequestParam(value = "username",required = false)String username,
+                                                   @RequestParam(value = "roleId",required = false)Long roleId,
+                                                   @RequestParam(value = "lastName",required = false)String lastName,
+                                                  @RequestParam(value = "page") int page,
+                                                  @RequestParam(value = "pageSize") int pageSize){
+        HttpStatus httpCode ;
+        Response resp = new Response();
+        Page<User> response = partnerUserService.findByClientId(firstName,phone,email,username,roleId,lastName, PageRequest.of(page, pageSize));
+        resp.setCode(CustomResponseCode.SUCCESS);
+        resp.setDescription("Record fetched successfully !");
+        resp.setData(response);
+        httpCode = HttpStatus.OK;
+        return new ResponseEntity<>(resp, httpCode);
+    }
 
 
 
