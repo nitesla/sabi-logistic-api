3e891619a1343cbedafa8b2488d4149df60ffa9e
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-11-25 02:17:42 +0100

    +++ Fixes

--- src/main/java/com/sabi/logistics/api/controllers/PaymentTermsController.java
+++ src/main/java/com/sabi/logistics/api/controllers/PaymentTermsController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @SuppressWarnings("All")
 @RestController
-@RequestMapping(Constants.APP_CONTENT +"paymentitems")
+@RequestMapping(Constants.APP_CONTENT +"paymentterms")
 public class PaymentTermsController {
 
 
