6900bd8db72f54485bc7e11571b095e2da237fc5
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2022-02-03 11:08:52 +0100

    Update

--- src/main/java/com/sabi/logistics/api/controllers/PricingConfigurationController.java
+++ src/main/java/com/sabi/logistics/api/controllers/PricingConfigurationController.java
@@ -56,7 +56,6 @@ public class PricingConfigurationController {
                                                                 @RequestParam(value = "stateId", required = false)Long stateId,
                                                                 @RequestParam(value = "locationPreference", required = false)String locationPreference,
                                                                 @RequestParam(value = "startingLocation", required = false)String startingLocation,
-                                                                @RequestParam(value = "destinationLocations", required = false)String destinationLocations,
                                                                 @RequestParam(value = "pricePerParameter", required = false)BigDecimal pricePerParameter,
                                                                 @RequestParam(value = "pricePerWeight", required = false)BigDecimal pricePerWeight,
                                                                 @RequestParam(value = "pricePerDistance", required = false)BigDecimal pricePerDistance,
@@ -65,7 +64,7 @@ public class PricingConfigurationController {
                                                  @RequestParam(value = "page") int page,
                                                  @RequestParam(value = "pageSize") int pageSize){
         return responseHelper
-                .buildResponse(pricingConfigurationService.findAll(partnerId, routeType, stateId, locationPreference, startingLocation, destinationLocations, pricePerParameter, pricePerWeight,
+                .buildResponse(pricingConfigurationService.findAll(partnerId, routeType, stateId, locationPreference, startingLocation, pricePerParameter, pricePerWeight,
                                 pricePerDistance, pricePerTime, hasPreferentialPricing, PageRequest.of(page, pageSize)),
                         HttpStatus.OK, "Record fetched successfully !");
     }
