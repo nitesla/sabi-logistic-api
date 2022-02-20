38699e2d694ef7ac2fd7c4e74bd954caa8bc5607
ekedon <ekene.a@spinel.consulting>
2022-02-20 12:15:02 +0100

    updates

--- src/main/SQL/1.0_shedluck.sql
+++ src/main/SQL/1.0_shedluck.sql
@@ -8,15 +8,12 @@ CREATE TABLE shedlock (
 
 
 
-CREATE TABLE UsersRole(
-  	userId BIGINT NOT NULL REFERENCES user (id),
-  	roleId BIGINT NOT NULL REFERENCES role (id),
-    createdOn            DATETIME     NULL ,
-    createdBy            BIGINT NULL  ,
-    lastUpdatedOn        DATETIME     NULL,
-    lastUpdatedBy        BIGINT     NULL,
-    CONSTRAINT PK_UsersRole PRIMARY KEY (userId,roleId)
-
- );
+CREATE TABLE IF NOT EXISTS `qasabilogisticapi`. shedlock (
+  name VARCHAR(64),
+  lock_until TIMESTAMP(3) NULL,
+  locked_at TIMESTAMP(3) NULL,
+  locked_by VARCHAR(255),
+  PRIMARY KEY (name)
+)
 
 
