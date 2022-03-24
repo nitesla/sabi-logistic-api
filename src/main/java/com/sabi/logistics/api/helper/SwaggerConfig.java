7d10b8d322938315e322b98470ea4c8ab027c29f
Afam Okonkwo <afam.o@spinel.consulting>
2022-03-24 10:27:48 +0100

    swagger web token Authorization feature

--- src/main/java/com/sabi/logistics/api/helper/SwaggerConfig.java
+++ src/main/java/com/sabi/logistics/api/helper/SwaggerConfig.java
@@ -5,11 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
@@ -25,6 +28,8 @@ public class SwaggerConfig {
 
                 .apis(RequestHandlerSelectors.basePackage("com.sabi.logistics.api"))
                 .paths(regex("/*.*")).build()
+                .securitySchemes(Arrays.asList(apiKey()))
+                .securityContexts(Arrays.asList(securityContext()))
                 .apiInfo(metaData());
     }
 
@@ -34,6 +39,22 @@ public class SwaggerConfig {
                 .license("Apache License Version 2.0").licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
                 .contact(new Contact("SABI Nig LTD", "www.xxxxxxx.com ", "info@xxxxxx.com")).build();
     }
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT","Authorization","Header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global","have access to every endpoint");
+        AuthorizationScope [] authorizationScopes = new AuthorizationScope[1];
+        return  Arrays.asList(new SecurityReference("JWT",authorizationScopes));
+    }
+
+
 
 
 
